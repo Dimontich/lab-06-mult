3ba3e7d7dff74f0e0d7e7195e20c72c2dbbe6262
CERZAR <stavropoltsev.t@gmail.com>
2019-12-09 18:06:16 +0300

    Finished

--- scripts/genenv.sh
+++ scripts/genenv.sh
@@ -3,6 +3,7 @@
 set -e
 
 echo "TRAVIS_REPO_SLUG=$TRAVIS_REPO_SLUG" > .env
+# shellcheck disable=SC2129
 echo "TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST" >> .env
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH" >> .env
 echo "TRAVIS_COMMIT=$TRAVIS_COMMIT" >> .env
--- sources/main.cpp
+++ sources/main.cpp
@@ -6,20 +6,15 @@
 #include "../include/HashFinder.h"
 
 int main(int argc, char *argv[]) {
-
     size_t threads_num = std::thread::hardware_concurrency();
     if (argc > 1)
         threads_num = boost::lexical_cast<size_t>(argv[1]);
-
     boost::log::add_file_log("NewLog.txt");
     boost::log::add_console_log(std::cout);
-
     std::vector<std::thread> threads;
     for (size_t i = 0; i < threads_num; i++)
         threads.emplace_back(HashFinder);
-        
     for (std::thread &thread : threads)
         thread.join();
-
     return 0;
 }
